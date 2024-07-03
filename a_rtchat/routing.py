79b3bd5bc8a933c18e0d59b02e9901c9ed1ac104
BredyByte <correodemimobil@gmail.com>
2024-07-03 15:21:09 +0200

    feat: Consumer is done. Chat works with websockets

--- a_rtchat/routing.py
+++ a_rtchat/routing.py
@@ -5,5 +5,5 @@ from a_rtchat.consumers import ChatroomConsumer
 
 
 websocket_urlpatterns = [
-    path("ws/chatroom/<chatroot_name>", ChatroomConsumer.as_asgi()),
+    path("ws/chatroom/<chatroom_name>", ChatroomConsumer.as_asgi()),
 ]

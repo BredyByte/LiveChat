9a9772a1a4645000b173807a734e4ab4dd18af5e
BredyByte <correodemimobil@gmail.com>
2024-07-04 12:10:24 +0200

    feat: Redis and Env. vars are setted up

--- a_core/settings.py
+++ a_core/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+from traitlets import default
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default="True")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 
@@ -91,11 +94,30 @@ TEMPLATES = [
 
 ASGI_APPLICATION = "a_core.asgi.application"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+ENVIRONMENT = config("ENVIRONMENT", default="development")
+
+if ENVIRONMENT == "development":
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        }
     }
-}
+elif ENVIRONMENT == "production":
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [
+                    (
+                        config("REDIS_HOST", default="localhost"),
+                        config("REDIS_PORT", default=6379),
+                    )
+                ],
+            },
+        },
+    }
+else:
+    raise ValueError("Invalid Environment name")
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
